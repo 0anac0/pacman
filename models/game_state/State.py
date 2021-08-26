94fff208bbf225f0fb530548f47fe5a72821be80
Ana Cláudia <ana.claudia@gocase.com.br>
2021-08-26 00:11:50 -0300

    small changes

--- models/game_state/State.py
+++ models/game_state/State.py
@@ -2,3 +2,12 @@ class State:
     def __init__(self, app):
         self.app = app
         self.screen = self.app.screen
+
+    def events(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
